2d0701da562bea6f8a476f54c6232f553517dbd4
pwh <cash3816.ee92@g2.nctu.edu.tw>
2017-09-23 09:22:39 -0700

    Add the use of CSV module

--- parsing_csv_file/main.py
+++ parsing_csv_file/main.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 DATADIR = ""
 DATAFILE = "beatles-diskography.csv"
@@ -22,15 +23,32 @@ def parse_file(datafile):
     return data
 
 
+'''
+Using CSV Module
+'''
+def parse_csv(datafile):
+    data = []
+    n = 0
+    with open(datafile,'r') as sd:
+        r = csv.DictReader(sd)
+        for line in r:
+            data.append(line)
+        return data
+
+
 def test():
     # a simple test of your implemetation
     datafile = os.path.join(DATADIR, DATAFILE)
     d = parse_file(datafile)
+    e = parse_csv(datafile)
     firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
     tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
 
     assert d[0] == firstline
     assert d[9] == tenthline
+    
+    for ele in e:
+        print(ele['Label'])
 
     
 test()
